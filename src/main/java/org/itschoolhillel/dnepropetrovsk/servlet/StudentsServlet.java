d47a742e25aef5f35e6c1e0018f6b83d78cb1283
hillel-java <svolkovsky.hillel@gmail.com>
2017-02-17 20:18:20 +0200

    new servlet functionality

--- src/main/java/org/itschoolhillel/dnepropetrovsk/servlet/StudentsServlet.java
+++
@@ -1,9 +0,0 @@
-package org.itschoolhillel.dnepropetrovsk.servlet;
-
-import javax.servlet.http.HttpServlet;
-
-/**
- * Created by stephenvolf on 05/02/17.
- */
-public class StudentsServlet extends HttpServlet {
-}
---
+++ src/main/java/org/itschoolhillel/dnepropetrovsk/servlet/UsersServlet.java
@@ -0,0 +1,17 @@
+package org.itschoolhillel.dnepropetrovsk.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by stephenvolf on 05/02/17.
+ */
+public class UsersServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
